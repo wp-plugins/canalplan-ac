23d7d241889be42e99b83dcfbb0272c12f677785
SteveAtty <SteveAtty@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-28 09:30:47 +0000

    Version 3.3 - map logic fix
    git-svn-id: https://plugins.svn.wordpress.org/canalplan-ac/tags/3.3@704938 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- canalplan/canalplan.php
+++ canalplan/canalplan.php
@@ -1,61 +1,52 @@
 <?php
+require_once( '../../../../wp-config.php');
+global $wpdb,$user_ID;
+
+if (strlen($_GET['match'])>0){
 $match=$_GET['match'];
+
+$cp_blog_id=intval($_GET['blogid']);
+if ($cp_blog_id<1) {$cp_log_id=1;}
+	$sql =$wpdb->prepare("select canalplan_id, place_name from ".CANALPLAN_CODES." where substr(place_name,1,".strlen($match).")=%s",$match);
+	$canalplaces = $wpdb->get_results($sql,ARRAY_A);
+    foreach($canalplaces as $canalplace) { print trim($canalplace['place_name']).'#'; }
+    $sql = $wpdb->prepare("select canalplan_id, place_name from ".CANALPLAN_ALIASES." where substr(place_name,1,".strlen($match).")=%s",$match);
+    $canalplaces = $wpdb->get_results($sql,ARRAY_A);
+    foreach($canalplaces as $canalplace) { print trim($canalplace['place_name']).'#'; }
+    $sql=$wpdb->prepare("select id, fullname from ".CANALPLAN_CANALS." where parent!='' and substr(fullname,1,".strlen($match).")=%s",$match);
+    $canalplaces = $wpdb->get_results($sql,ARRAY_A);
+    foreach($canalplaces as $canalplace) { print trim($canalplace['fullname']).'#'; }
+}
 $place=$_GET['place'];
-$cp_blog_id=$_GET['blogid'];
-require("../../../../wp-config.php");
-if ($cp_blog_id=="undefined") {unset($cp_blog_id);}
-if (strlen($match)>0){
-$sql="set names 'utf8';";
-$zed = mysql_query($sql);
-	//	$canalplaces = mysql_query($sql);
-//	while ($canalplace = mysql_fetch_array($canalplaces)) { print trim($canalplace[place_name]).'#'; }
-	$sql = "select canalplan_id, place_name from ".CANALPLAN_CODES." where substr(place_name,1,".strlen($match).")='".$match."'";
-	$canalplaces = mysql_query($sql);
-	while ($canalplace = mysql_fetch_array($canalplaces)) { print trim($canalplace[place_name]).'#'; }
-        $sql = "select canalplan_id, place_name from ".CANALPLAN_ALIASES." where substr(place_name,1,".strlen($match).")='".$match."'";
-        $canalplaces = mysql_query($sql);
-        while ($canalplace = mysql_fetch_array($canalplaces)) { print trim($canalplace[place_name]).'#'; }
-        $sql = "select id, fullname from ".CANALPLAN_CANALS." where parent!='' and substr(fullname,1,".strlen($match).")='".$match."'";
-        $canalplaces = mysql_query($sql);
-        while ($canalplace = mysql_fetch_array($canalplaces)) { print trim($canalplace[fullname]).'#'; }
-
-}
 if (strlen($place)>0){
-$place=trim(urldecode($place));
-// If we have a blog_id passed in then we need to get back the favourites and then the main rows but remove any common names
-  if (isset($cp_blog_id)) {
-$sql="set names 'utf8';";
-$zed = mysql_query($sql);
-	  // Get Check Favourites
-		$sql = 'select  canalplan_id from '.CANALPLAN_FAVOURITES.' where place_name="'.$place.'" and blog_id='.$cp_blog_id;
-		$canalplaces = mysql_query($sql);
-		while ($canalplace = mysql_fetch_array($canalplaces)) { print "X".trim($canalplace[canalplan_id]); }
+	$place=trim(urldecode($place));
+	$cp_blog_id=intval($_GET['blogid']);
+	if ($cp_blog_id=="undefined") {unset($cp_blog_id);}
+	// If we have a blog_id passed in then we need to get back the favourites and then the main rows but remove any common names
+	  if (isset($cp_blog_id)) {
+		// Get Check Favourites
+		$sql = $wpdb->prepare('select  canalplan_id from '.CANALPLAN_FAVOURITES.' where place_name=%s and blog_id=%d', $place, $cp_blog_id);
+		$canalplaces = $wpdb->get_results($sql,ARRAY_A);
+		foreach($canalplaces as $canalplace) {  print "X".trim($canalplace[canalplan_id]); }
 		// Now check the rest
-		$sql = 'select  cc.canalplan_id from '.CANALPLAN_CODES.' cc where  cc.place_name="'.$place.'" and  cc.canalplan_id not in (select cf.canalplan_id from '.CANALPLAN_FAVOURITES.' cf where cf.blog_id='.$cp_blog_id.' and cf.place_name="'.$place.'")';
-		$canalplaces = mysql_query($sql);
-		while ($canalplace = mysql_fetch_array($canalplaces)) { print "X".trim($canalplace[canalplan_id]); }
-                $sql = 'select  cc.canalplan_id from '.CANALPLAN_ALIASES.' cc where  cc.place_name="'.$place.'" and  cc.canalplan_id not in (select cf.canalplan_id from '.CANALPLAN_FAVOURITES.' cf where cf.blog_id='.$cp_blog_id.' and cf.place_name="'.$place.'")';
-                $canalplaces = mysql_query($sql);
-                while ($canalplace = mysql_fetch_array($canalplaces)) { print "X".trim($canalplace[canalplan_id]); }
-	        $sql = "select id from ".CANALPLAN_CANALS." where fullname='".$place."'";
-	        $canalplaces = mysql_query($sql);
-	        while ($canalplace = mysql_fetch_array($canalplaces)) { print "W".trim($canalplace[id]); }
+		$sql = $wpdb->prepare('select  cc.canalplan_id from '.CANALPLAN_CODES.' cc where  cc.place_name=%s and  cc.canalplan_id not in (select cf.canalplan_id from '.CANALPLAN_FAVOURITES.' cf where cf.blog_id=%d and cf.place_name=%s)', $place, $cp_blog_id,$place);
+		$canalplaces = $wpdb->get_results($sql,ARRAY_A);
+		foreach($canalplaces as $canalplace) {  print "X".trim($canalplace['canalplan_id']); }
+		$sql = $wpdb->prepare('select  cc.canalplan_id from '.CANALPLAN_ALIASES.' cc where  cc.place_name=%s and  cc.canalplan_id not in (select cf.canalplan_id from '.CANALPLAN_FAVOURITES.' cf where cf.blog_id=%d and cf.place_name=%s)', $place, $cp_blog_id,$place);
+		$canalplaces = $wpdb->get_results($sql,ARRAY_A);
+		foreach($canalplaces as $canalplace) {  print "X".trim($canalplace['canalplan_id']); }
+		$sql = $wpdb->prepare("select id from ".CANALPLAN_CANALS." where fullname=%s",$place);
 
+		$canalplaces = $wpdb->get_results($sql,ARRAY_A);
+		foreach($canalplaces as $canalplace) { print "W".trim($canalplace[id]); }
+		}
+		else {
+		$sql = $wpdb->prepare("select canalplan_id from ".CANALPLAN_CODES." where place_name=%s",stripslashes($place));
+		$canalplaces = $wpdb->get_results($sql,ARRAY_A);
+		foreach($canalplaces as $canalplace) { print "".trim($canalplace['canalplan_id']); }
+		$sql = $wpdb->prepare("select canalplan_id from ".CANALPLAN_ALIASES." where place_name=%s",stripslashes($place));;
+		$canalplaces = $wpdb->get_results($sql,ARRAY_A);
+		foreach($canalplaces as $canalplace) { print "".trim($canalplace['canalplan_id']); }
 	}
-	else {
-$sql="set names 'utf8';";
-$zed = mysql_query($sql);
-	$sql = "select canalplan_id from ".CANALPLAN_CODES." where place_name='".$place."'";
-	$canalplaces = mysql_query($sql);
-	while ($canalplace = mysql_fetch_array($canalplaces)) { print "".trim($canalplace[canalplan_id]); }
-	$sql = "select canalplan_id from ".CANALPLAN_ALIASES." where place_name='".$place."'";
-	$canalplaces = mysql_query($sql);
-	while ($canalplace = mysql_fetch_array($canalplaces)) { print "".trim($canalplace[canalplan_id]); }
-	// We Really don't want people adding canals to their favorites.... well not yet
-	#$sql = "select id from canals where fullname='".$place."'";
-	#$canalplaces = mysql_query($sql);
-	#while ($canalplace = mysql_fetch_array($canalplaces)) { print "".trim($canalplace[id]); }
 }
-}
-#}
 ?>
