ee2d407723671111ca45df6a68b662a0e85fdc73
SteveAtty <SteveAtty@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-05 09:41:20 +0000

    Version 3.8 : New shortcodes and better RSS feed support
    git-svn-id: https://plugins.svn.wordpress.org/canalplan-ac/tags/3.8@782928 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- canalplan/canalplanfunctions.js
+++ canalplan/canalplanfunctions.js
@@ -12,7 +12,7 @@ else if (window.ActiveXObject)
   }
 if (xmlhttp!=null)
   {
-  xmlhttp.open("GET","/wp-content/plugins/canalplan/canalplan/canalplan.php?place="+t+"&blogid="+bid,false)
+  xmlhttp.open("GET",wpcontent+"/canalplan-ac/canalplan/canalplan.php?place="+t+"&blogid="+bid,false)
   xmlhttp.send(false)
   xxx=xmlhttp.responseText.replace("\n","");
 xxx=xxx.replace(" ","");
@@ -33,7 +33,7 @@ junk3=document.getElementById("content");
 tagcode=junk2.value;
 //var myField;
 tagextend=code_id.substring(0,1);
-code_id=code_id.substring(1);  
+code_id=code_id.substring(1);
 if (tagextend=="W") {tagcode=tagcode+tagextend};
 tinyMCE.execCommand('mceReplaceContent', false, '[['+ tagcode +':' + tag + '|' + code_id +  ']]' + ' ');
 // The next bit works if you are in HTML raw mode
@@ -41,3 +41,11 @@ junk3.value=junk3.value+' [['+ tagcode +':' + tag + '|' + code_id +  ']]' + ' '
 return;
 }
 
+function getCanalRoute(tag) {
+junk2=document.getElementById("routetagtypeID");
+junk3=document.getElementById("content");
+tinyMCE.execCommand('mceReplaceContent', false, '[['+ junk2.value +':' + tag +  ']]' + ' ');
+// The next bit works if you are in HTML raw mode
+junk3.value=junk3.value+' [['+ junk2.value +':' + tag +  ']]' + ' '
+return;
+}
